51bebfc820834e46e037721fa22de09ac1549730
okechukwufranklin <151369058+okechukwufranklin@users.noreply.github.com>
2024-03-04 12:27:42 +0100

    .

---
+++ task/Number_list.py
@@ -0,0 +1,10 @@
+def Number_list():
+    my_list = []
+    for number in range(1, 16):
+        my_list = number
+
+    return my_list
+
+def Duplicate_list():
+    my_list =[]
+    Number_list() * 2
